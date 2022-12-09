3414a533ecbbce8a9824c5c54acd0a97b72021f2
fitmeat <eenot825@gmail.com>
2022-12-09 16:08:31 +0300

    Статистика 1.0

--- doptask1/Program.cs
+++ doptask1/Program.cs
@@ -13,24 +13,24 @@ CreateArray(arr);
 Console.WriteLine($"[{string.Join(", ", arr)}]");
 int threes = 0;
 int fours = 0;
+
 for(int i = 0; i<arr.Length; i++){
     if(arr[i]%2 != 0){
         threes++;
     } else fours++;
 }
-Console.WriteLine(threes);
-Console.WriteLine(fours);
-int[] arr2 = new int[threes];
-int[] arr3 = new int[fours];
-for(int i =0; i<arr.Length-1; i++){
+
+int[] arr2 = new int[arr.Length];
+int[] arr3 = new int[arr.Length];
+
+for(int i =0; i<arr.Length; i++){
    if(arr[i]%2 != 0){
-    for(int j = 0; j<arr2.Length; j++){
-        arr2[j] = arr[i];
-    }
-   } else 
-   for(int k = 0; k<arr3.Length; k++){
-    arr3[k] = arr[i];
-   }
+        arr2[i] = arr[i];
+    } else arr3[i] = arr[i];
 }
 Console.WriteLine($"Неченые числа: {string.Join(", ", arr2)}");
 Console.WriteLine($"Четные числа: {string.Join(", ", arr3)}");
+if(threes>fours){
+    Console.WriteLine("NO"); 
+} else Console.WriteLine("YES");
+
